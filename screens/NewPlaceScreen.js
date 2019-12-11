9325a8212b1a5a91d879d8f721450a433ff50b9f
Jesse Krim <jesse.krim@gmail.com>
2019-12-11 16:20:42 -0500

    Set up basic input form on NewPlace screen

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,16 +1,64 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  ScrollView,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet
+} from 'react-native';
+import { HeaderTitle } from 'react-navigation-stack';
+import Colors from '../constants/Colors';
 
 const NewPlaceScreen = props => {
+  const [title, setTitle] = useState('');
+
+  const titleChangeHandler = text => {
+    setTitle(text);
+  };
+
+  const savePlaceHandler = () => {};
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={title}
+        />
+        <Button
+          title='Save Place'
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
+NewPlaceScreen.navigationOptions = navData => {
+  return {
+    HeaderTitle: 'Add Place'
+  };
+};
+
 const styles = StyleSheet.create({
-  // style
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
 });
 
 export default NewPlaceScreen;
